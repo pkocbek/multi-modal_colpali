19fbcd3bfdeed238dbb470db5669292d9216b1b0
pkocbek <54047853+pkocbek@users.noreply.github.com>
2025-11-15 20:39:54 +0100

    small fixes

---
+++ 00_run_docker_old_models.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+export $(xargs < .env)
+
+# Ensure you have the correct permissions to run docker,
+# you might need to run 'newgrp docker' in your shell before this script.
+# run only one or two models
+docker run --gpus all -it --rm \
+    --name biomed_Llama_VL \
+    -v "${HF_DIR}:/root/.cache/huggingface" \
+    --env "HUGGING_FACE_HUB_TOKEN=${HUGGING_FACE_HUB_TOKEN}" \
+    --env TRANSFORMERS_OFFLINE=1 \
+    --env VLLM_RPC_TIMEOUT=180000 \
+    --env HF_DATASET_OFFLINE=1 \
+    -p 8010:8000 \
+    --ipc=host \
+    vllm/vllm-openai:latest \
+    --model AdaptLLM/biomed-Llama-3.2-11B-Vision-Instruct \
+    --gpu-memory-utilization 0.75 --max_model_len 32000 --max_num_seqs 16 \
+    --enforce_eager --enable-sleep-mode 
+#     --disable-frontend-multiprocessing \
+#     --limit_mm_per_prompt 'image=1'
+
+docker run --gpus all -it --rm \
+    --name biomed_qwenVL \
+    -v "${HF_DIR}:/root/.cache/huggingface" \
+    --env VLLM_RPC_GET_DATA_TIMEOUT_MS=1800000 \
+    --env "HUGGING_FACE_HUB_TOKEN=${HUGGING_FACE_HUB_TOKEN}" \
+    --env TRANSFORMERS_OFFLINE=1 \
+    --env HF_DATASET_OFFLINE=1 \
+    -p 8005:8000 \
+    --ipc=host \
+    vllm/vllm-openai:latest \
+    --model AdaptLLM/biomed-Qwen2-VL-2B-Instruct  \
+    --gpu_memory_utilization 0.7 \
+    --max_model_len 32000 --enable-sleep-mode \
+    --enforce_eager
+#     #kv_cacher for 0.25 is 14,9 GB for cache)
+   # --shm-size 3g \
+
+docker run --gpus all -it --rm \
+    --name biomed_LLaVA \
+    -v "${HF_DIR}:/root/.cache/huggingface" \
+    --env "HUGGING_FACE_HUB_TOKEN=${HUGGING_FACE_HUB_TOKEN}" \
+    --env "TRANSFORMERS_OFFLINE=1" \
+    --env "HF_DATASET_OFFLINE=1" \
+    -p 8001:8000 \
+    --ipc=host \
+    vllm/vllm-openai:latest \
+    --model AdaptLLM/biomed-LLaVA-NeXT-Llama3-8B \
+    --gpu_memory_utilization 0.4 --max_model_len 8192 --enable-sleep-mode
+# # # #     # kv_cache for 0.25 is 3,18GBd
